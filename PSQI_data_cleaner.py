c22b33a25a45f73e642b252597d738c20cd09aa5
CAJG73 <cjimenez@sfu.ca>
2026-06-09 17:00:59 -0700

    Added correction rules for BRS participants with errors on their reported IDs. Additional functions were created to handle the ID changes, while some functions that were no longer needed were removed.

--- PSQI_data_cleaner.py
+++ PSQI_data_cleaner.py
@@ -2,9 +2,85 @@ import pandas as pd
 import re
 from datetime import datetime
 
-# Load the TSV file into a pandas DataFrame with the correct encoding
-file_path = '/Users/brs/Documents/psqi_downloads/desc-summary_date-250303_psqi.tsv'
-df = pd.read_csv(file_path, sep='\t', encoding='ISO-8859-1')  # Try 'ISO-8859-1' encoding
+IDs_correction_rules = [
+
+    {
+        "wrong_id": 'BRS0012',
+        "correct_id":'BRS0011',
+        "registered_date": "02/12/2024",
+        "reason": "Typo in registered ID",
+    },
+
+    {
+        "wrong_id": 'BRS0072',
+        "correct_id":'BRS0074',
+        "registered_date": "27/02/2025",
+        "reason": "Typo in registered ID",
+    },
+
+    {
+        "wrong_id": 'BRS0262',
+        "correct_id":'BRS0162',
+        "registered_date": "30/04/2025",
+        "reason": "Typo in registered ID",
+    },
+
+    {
+        "wrong_id": 'BRS0533',
+        "correct_id":'BRS0553',
+        "registered_date": "12/12/2025",
+        "reason": "Typo in registered ID",
+    },
+
+    {
+        "wrong_id": 'BRS0000',
+        "correct_id":'BRS0575',
+        "registered_date": "02/04/2026",
+        "reason": "Typo in registered ID",
+    }
+]
+
+
+def normalize_ids(value: str) -> str:
+    return (
+        str(value)
+        .strip()
+        .upper()
+        .replace(" ", "")
+    )
+
+
+def apply_correct_id(df, id_col='QID1', date_col='QID2', date_format='%d/%m/%Y'):
+    df = df.copy()
+    
+    df["raw_id"] = df[id_col].astype(str)
+
+    df["fixed_id"] = df[id_col].apply(normalize_ids)
+
+    df["id_resolution_reason"] = "original"
+
+    df[date_col] = pd.to_datetime(df[date_col], dayfirst=True, errors="coerce").dt.normalize()
+
+    for rule in IDs_correction_rules:
+        rule_date = pd.to_datetime(rule["registered_date"], dayfirst=True). normalize()
+
+        print(f"Wrong ID: {rule["wrong_id"]}")
+        print(f"Correct ID: {rule["correct_id"]}")
+        print(f"Date: {rule["registered_date"]}")
+
+        mask = (
+            (df[id_col] == normalize_ids(rule["wrong_id"])) &
+            (df[date_col] == rule_date)
+        )
+
+        print(df[mask])
+
+        df.loc[mask, "fixed_id"] = normalize_ids(rule["correct_id"])
+        df.loc[mask, "id_resolution_reason"] = rule["reason"]
+
+    df[date_col] = df[date_col].dt.strftime(date_format)
+
+    return df
 
 # Define a function to check if a participant ID follows the correct format
 def is_valid_participant_id(participant_id):
@@ -13,28 +89,42 @@ def is_valid_participant_id(participant_id):
         return bool(re.match(r'^BRS\d{4}$', participant_id))
     return False
 
+# Load the TSV file into a pandas DataFrame with the correct encoding
+file_path = '/Users/brs/Documents/psqi_downloads/desc-summary_date-250303_psqi.tsv'
+df = pd.read_csv(file_path, sep='\t', encoding='ISO-8859-1')  # Try 'ISO-8859-1' encoding
+print(f"Initial number of rows: {len(df)}")
+
+fixed_df = apply_correct_id(df)
+
+print(fixed_df.columns.tolist())
+
+export_df = fixed_df.copy()
+export_df['QID1'] = export_df['fixed_id']
+
+export_df = export_df.drop(columns=['raw_id','fixed_id', 'id_resolution_reason'])
+
+# List of IDs to be excluded
+excluded_IDs = ['BRS0197', 'BRS0307', 'BRS0376']
+
 # Remove rows with invalid participant IDs
-df = df[df.iloc[:, 10].apply(is_valid_participant_id)]  # Column 11 is index 10 in Python
+export_df = export_df[export_df.iloc[:, 10].apply(is_valid_participant_id)]  # Column 11 is index 10 in Python
+print(f"After removing invalid participant IDs: {len(export_df)}")
+
+# Remove rows that match with excluded IDs
+export_df = export_df[~export_df.iloc[:, 10].isin(excluded_IDs)].reset_index(drop=True)
+print(f"After excluded participant IDs: {len(export_df)}")
 
 # Remove rows with participant ID "BRS9999"
 df = df[df.iloc[:, 10] != 'BRS9999']
+print(f"After removing BRS9999: {len(export_df)}")
 
-# Remove rows where participant ID is "BRS1234" and the date is before March 1st, 2025
-def filter_brs1234_date(row):
-    if row.iloc[10] == 'BRS1234':  # Access the 11th column by position (index 10)
-        # Convert the date string to a datetime object
-        try:
-            date = datetime.strptime(row.iloc[11], '%d/%m/%Y')  # Access the 12th column by position (index 11)
-            # Compare the date with March 1st, 2025
-            return date >= datetime(2025, 3, 1)
-        except ValueError:
-            # If the date conversion fails, return False (remove the row)
-            return False
-    return True
+export_df = export_df[export_df.iloc[:, 10] != 'BRS1234']  # Apply the filtering function row-wise
+print(f"After filtering BRS1234: {len(export_df)}")
 
-df = df[df.apply(filter_brs1234_date, axis=1)]  # Apply the filtering function row-wise
+export_df = export_df.drop_duplicates(subset='QID1', keep='last', inplace=False)
 
-# Overwrite the original file with the cleaned data
-df.to_csv(file_path, sep='\t', index=False)
+# Save cleaned data to a new TSV file with UTF-8 encoding
+output_file = file_path.replace(".csv", ".tsv")
+export_df.to_csv(output_file, sep="\t", index=False, encoding="utf-8")
 
 print("Data cleaning complete. Original file has been overwritten.")
