41e2d64d6eac4f20d6cfcd0584307291d3b88ad6
CAJG73 <cjimenez@sfu.ca>
2026-09-02 16:13:05 -0700

    Update data_completeness_data_cleaner.py
    Forcing int type for numeric columns, and fixing the -1 convention for excluded IDs on string column in data completeness file

--- data_completeness_data_cleaner.py
+++ data_completeness_data_cleaner.py
@@ -1,24 +1,29 @@
 import pandas as pd
 # Read the CSV file without specifying dtype initially
-file_path = '/Users/cjimenez/Documents/BRS/DataManagement/Data_Transfer/Completeness-files/data_completeness_downloads/data_completeness_date-20260525.csv'
+file_path = '/Users/cjimenez/Documents/BRS/DataManagement/Data_Transfer/Completeness-files/data_completeness_downloads/data_completeness_date-20260901.csv'
 
 # List of IDs to be removed
 excluded_IDs = ['sub-BRS0197', 'sub-BRS0307', 'sub-BRS0376']
 
 # Read the CSV 
 df = pd.read_csv(file_path)
+
+df.iloc[:, 2:13] = df.iloc[:, 2:13].fillna(0).astype(int)
 print(f"Before removing excluded participant IDs: {len(df)}")
 
 # Define column indexes for anonymizing excluded rows
 id_idx = 0
 datetime_idx = 1
-columns_range = range(2, 14)
+num_columns_range = range(2, 13)
 
 # Create mask based on ID column
 mask = df.iloc[:, id_idx].isin(excluded_IDs)
 
-# Apply -1 to selected non-datetime columns
-df.iloc[mask, columns_range] = -1
+# Apply -1 to selected numeric and non-datetime columns
+df.iloc[mask, num_columns_range] = -1
+
+# Apply -1 to string column
+df.iloc[mask, 13] = '-1'
 
 # Apply NaT to datetime column
 df.iloc[mask, datetime_idx] = pd.NaT
